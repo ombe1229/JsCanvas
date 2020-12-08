94abbb99113be28a2ceb29d75fd99e41fcb2de08
ombe <h3236516@gmail.com>
2020-12-08 19:52:54 +0900

    색상 변경 기능 추가

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
 
 canvas.width = 700;
 canvas.height = 700;
 
-ctx.strokoStyle = "#2c2c2c"
+ctx.strokeStyle = "#2c2c2c"
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -31,8 +32,9 @@ function onMouseMove(event) {
     }
 }
 
-function onMouseDown(event) {
-    painting = true;
+function handleColorClick(event) {
+    const color =  event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
 }
 
 if (canvas) {
@@ -41,3 +43,7 @@ if (canvas) {
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color =>
+    color.addEventListener("click", handleColorClick)
+);
