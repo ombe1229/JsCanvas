b52ef724eeb724185d9c8310aa3464ea55d9dd0d
ombe1229 <h3236516@gmail.com>
2020-12-09 14:32:00 +0900

    github 버튼 추가

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 const saveBtn = document.getElementById("jsSave");
+const githubBtn = document.getElementById("jsGithub");
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 700;
@@ -81,6 +82,12 @@ function handleSaveClick() {
     link.click();
 }
 
+function handleGithubClick() {
+    window.open("https://github.com/ombe1229/JsPaint", "_blank");
+}
+
+
+
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
@@ -104,4 +111,8 @@ if(mode){
 
 if(saveBtn){
     saveBtn.addEventListener("click", handleSaveClick);
+}
+
+if(githubBtn){
+    githubBtn.addEventListener("click", handleGithubClick);
 }
